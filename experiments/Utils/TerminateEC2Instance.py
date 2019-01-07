71b8366f8d7046848cc50b29329e2cde07aea7a5
William Zhang <xz1792@gmail.com>
2019-01-08 10:53:59 +1100

    remove access info from code

--- experiments/Utils/TerminateEC2Instance.py
+++ experiments/Utils/TerminateEC2Instance.py
@@ -5,11 +5,7 @@ from botocore.exceptions import ClientError
 
 instance_id = sys.argv[1]
 
-ec2 = boto3.client(
-    'ec2',
-    aws_access_key_id= "AKIAJGXBXJ2SNAVVJPUQ",
-    aws_secret_access_key= "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",
-                   )
+ec2 = boto3.client('ec2')
 
 # Do a dryrun first to verify permissions
 try:

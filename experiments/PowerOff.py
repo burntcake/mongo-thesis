dc54bcfc116c65b08336f5ddc378358469fb1b54
William Zhang <xz1792@gmail.com>
2019-01-18 16:07:15 +1100

    remove terminate script add response printout to poweroff command

--- experiments/PowerOff.py
+++ experiments/PowerOff.py
@@ -4,8 +4,10 @@ client = boto3.client('ssm')
 instance_id = [sys.argv[1]]
 print(instance_id)
 commands = ['shutdown -f']
-client.send_command(
+response = client.send_command(
     DocumentName="AWS-RunShellScript",
     Parameters={'commands': commands},
     InstanceIds=instance_id,
 )
+
+print(response)

3545c86fd4297d9e3acaac9630685e738b4aae76
William Zhang <xz1792@gmail.com>
2019-01-18 16:12:14 +1100

    add time parameter to shutdown command

--- experiments/PowerOff.py
+++ experiments/PowerOff.py
@@ -3,7 +3,7 @@ import sys
 client = boto3.client('ssm')
 instance_id = [sys.argv[1]]
 print(instance_id)
-commands = ['shutdown -f']
+commands = ['shutdown -f now']
 response = client.send_command(
     DocumentName="AWS-RunShellScript",
     Parameters={'commands': commands},

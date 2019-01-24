55c86675263a1a365b3080d64001491ac18b33fc
William Zhang <xz1792@gmail.com>
2019-01-24 12:10:08 +1100

    print

--- experiments/Program.cs
+++ experiments/Program.cs
@@ -86,7 +86,7 @@ namespace MongoDBExperiments
             {
                 ec2InstanceId.Add(serversAndInstanceIds.Item1.ToString(), serversAndInstanceIds.Item2);
             }
-            Console.WriteLine(ec2InstanceId.ToString());
+            Console.WriteLine(ec2InstanceId.ToJson());
             builder.RegisterInstance(ec2InstanceId).As<IDictionary<string, string>>();
 
             var container = builder.Build();

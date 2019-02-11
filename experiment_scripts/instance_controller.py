1ee307966a40517b1b04fd69e8d0afacb9bdd89f
DaweiTao <37278415+DaweiTao@users.noreply.github.com>
2019-02-11 15:25:18 +1100

    bug fixed

--- experiment_scripts/instance_controller.py
+++ experiment_scripts/instance_controller.py
@@ -23,11 +23,7 @@ class MongoReplicaSet:
 
     # start all aws instances
     def start_all(self):
-        print("!!!!!!", self.instance_id_list)
         for instance_id in self.instance_id_list:
-            print("1111111", instance_id)
-        for instance_id in self.instance_id_list:
-            print("idididiid", instance_id)
             try:
                 self.client.start_instances(InstanceIds=[instance_id], DryRun=True)
             except ClientError as e:
